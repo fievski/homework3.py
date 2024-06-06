f9394fe40b960c6f337289975b25bea29ef23ff2
fievski <fievski@yandex.ru>
2024-06-06 12:09:03 +0600

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Sam'  # Динамическая типизация
+print ('Name',':', name)
+age = 25
+print ('Age', ':', age)
+new_age = (0 + 1)
+print ('New age', ':', age + new_age)
+is_student = True
+print ('Is student', ':', True)
